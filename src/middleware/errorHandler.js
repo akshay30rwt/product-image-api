c11cd637505d91e10477312977ca4a731355bc5b
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-13 14:48:46 +0530

    feat: add global error handler

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+const errorHandler = (err, req, res, next) => {
+    console.log(err.stack);
+
+    const message = err.message || 'Internal Server Error';
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        error: {
+            message,
+            statusCode
+        }
+    });
+};
+
+export { errorHandler };
