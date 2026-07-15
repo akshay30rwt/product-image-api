b444c8eb1754d5b207bfa754cc748f3c0688da61
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-15 17:50:33 +0530

    feat: validate data when updating product

--- src/validators/productValidator.js
+++ src/validators/productValidator.js
@@ -7,4 +7,11 @@ const productSchema = Joi.object({
     category: Joi.string().valid('electronics', 'clothing', 'kitchen', 'beauty', 'books', 'sports', 'toys', 'grocery', 'other').required() 
 });
 
-export { productSchema };
+const updateProductSchema = Joi.object({
+    name: Joi.string().min(2).max(50),
+    description: Joi.string().min(3).max(300),
+    price: Joi.number().min(0).max(1000000),
+    category: Joi.string().valid('electronics', 'clothing', 'kitchen', 'beauty', 'books', 'sports', 'toys', 'grocery', 'other')
+});
+
+export { productSchema, updateProductSchema };
